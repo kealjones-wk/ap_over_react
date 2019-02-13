af01808c50983902a286ebf2dc82956d219c3251
Joe Bingham <joe.bingham@workiva.com>
2019-02-13 15:42:26 -0700

    Add 11 extra credit 2

--- lib/src/app_components/fullpage.dart
+++ lib/src/app_components/fullpage.dart
@@ -31,6 +31,7 @@ import 'package:ap_over_react/src/exercises-final/10/extra/3/usage.dart' deferre
 //import 'package:ap_over_react/src/exercises/10/usage.dart' deferred as exercise_10;
 import 'package:ap_over_react/src/exercises-final/11/usage.dart' deferred as exercise_11_final;
 import 'package:ap_over_react/src/exercises-final/11/extra/1/usage.dart' deferred as exercise_11_1_final;
+import 'package:ap_over_react/src/exercises-final/11/extra/2/usage.dart' deferred as exercise_11_2_final;
 import 'package:ap_over_react/src/exercises/11/usage.dart' deferred as exercise_11;
 //import 'package:ap_over_react/src/exercises/05/usage.dart' deferred as exercise_05;
 
@@ -258,6 +259,16 @@ class FullPageComponent
           if (props.type == 'final') {
             await exercise_11_1_final.loadLibrary();
             return exercise_11_1_final.Usage;
+          } else {
+            return NotReady;
+          }
+        }
+        break;
+      case '11.2':
+        {
+          if (props.type == 'final') {
+            await exercise_11_2_final.loadLibrary();
+            return exercise_11_2_final.Usage;
           } else {
             return NotReady;
           }
