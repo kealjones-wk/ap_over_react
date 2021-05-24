617237cde483f25b1d747552932d704fbd3a7b5e
vivienfrancespabuna-wk <vivienfrances.pabuna@workiva.com>
2021-05-24 11:45:22 -0700

    Added questions to exercise 1

--- lib/src/exercises/ap_01.dart
+++ lib/src/exercises/ap_01.dart
@@ -1,38 +1,40 @@
 // 01: Building the toggle component
 
 import 'package:over_react/over_react.dart';
-// 🐨 uncomment this import to get the switch component.
-// It takes an `onClick` and an `on` prop
 import 'package:ap_over_react/switch.dart';
 
 part 'ap_01.over_react.g.dart';
 
-// ignore: undefined_identifier
+//-- using a component from the library 🤔
+//-- what does _$Toggle mean❓
 UiFactory<ToggleProps> Toggle = castUiFactory(_$Toggle);
 
+//-- mixins are good for shared behavior 🤔
 mixin ToggleProps on UiProps {
-  /// Callback that returns `state.isOn` when the toggle switches;
+  // Callback that returns `state.isOn` when the toggle switches;
   void Function(bool isOn) onToggle;
+  //-- what is this structure❓
+  //-- (return type) Function(____) ____ ❓
 }
 
 mixin ToggleState on UiState {
-  // Whether the toggle is On or Off
+  // ToggleState has a bool value (isOn) ✅
   bool isOn;
 }
 
 class ToggleComponent extends UiStatefulComponent2<ToggleProps, ToggleState> {
 
-  // 🐨 this toggle component is going to need to have state for `on`
-  @override
-  get initialState => newState()..isOn = false;
+  // 🐨 
+  @override //-- why the override? is there a default initialState❓
+  get initialState => newState()..isOn = false; // this toggle component is going to need to have state for `on` ✅
 
   // You'll also want a method to handle when the switch is clicked
   // which will update the `on` state and call the `onToggle` prop
   // with the new `on` state.
   void toggle(_) {
-      // 💯 Use a state updater function for `newState` to avoid issues with batching
-      // 💰 setState(newState, callback)
-      setState((newState()..isOn = !state.isOn), () {
+      // Use a state updater function for `newState` to avoid issues with batching
+      // setState(newState, callback)
+      setState((newState()..isOn = !state.isOn), () { // is this callback an anonymous function? (no return type or name)❓
       // The `callback` should be where you call `props.onToggle(state.isOn)`
       props.onToggle(state.isOn);
     });
@@ -40,19 +42,17 @@ class ToggleComponent extends UiStatefulComponent2<ToggleProps, ToggleState> {
 
   @override
   render() {
-    // 🐨 here you'll want to return the switch with the `on` and `onClick` props
+    // here you'll want to return the switch with the `on` and `onClick` props
     return (Switch()
-      ..isOn = state.isOn
+      ..isOn = state.isOn // '..' replaces something like 'this.' right (cascade)❓
       ..onClick = toggle
     )();
   }
 }
 
-// Don't make changes to the Usage component. It's here to show you how your
-// component is intended to be used and is used in the tests.
-// You can make all the tests pass by updating the Toggle component.
+// what does Usage essentially do❓
 UiFactory<UsageProps> Usage = uiFunction(
-  (props) {
+  (props) { //-- ?? = null-aware operator ✅
     final onToggle = props.onToggle ?? (isOn) => print('onToggle $isOn');
     return (Toggle()..onToggle = onToggle)();
   },
