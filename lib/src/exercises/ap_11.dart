f7eba70cf0ee3d92658d045019555856c0eb600c
vivienfrancespabuna-wk <vivienfrances.pabuna@workiva.com>
2021-05-26 16:14:45 -0700

    Exercise 11

--- lib/src/exercises/ap_11.dart
+++ lib/src/exercises/ap_11.dart
@@ -40,9 +40,11 @@ class ToggleComponent extends UiStatefulComponent2<ToggleProps, ToggleState> {
   render() {
     //Before working on the Toggle component in this exercise, it will cause errors
     //When the component is functional (not necessarily complete), it will render
-    return (props.children.single(SharedTogglePropsMapView()
+    return (ToggleContext.Provider()
+      ..value = (SharedTogglePropsMapView()
       ..isOn = state.isOn
-      ..toggle = toggle))();
+      ..toggle = toggle)
+      )(props.children);
   }
 }
 
